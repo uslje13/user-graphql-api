e50690fccf16387a9769e7a3dc071148baa19659
Uslje <147374020+uslje13@users.noreply.github.com>
2024-09-27 16:03:39 +0200

    refactor: separeted queiries and mutations

--- UserAPI/index.js
+++ UserAPI/index.js
@@ -1,35 +1,17 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "./schema.js";
-import { v4 as uuidv4 } from 'uuid';
-import db from './_db.js'
+import { getAllUsersResolver } from "./resolvers/queries/getAllUsersResolver.js";
+import { addUserResolver } from "./resolvers/mutations/addUserResolver.js";
+import { deleteUserResolver } from "./resolvers/mutations/deleteUserResolver.js";
 
 const resolvers = {
-    Query: {
-        users() {
-            return db.users
-        },
-    },
+    Query: getAllUsersResolver,
     Mutation: {
-        addUser(_, { name, email }) {
-            const newUser = {
-                id: uuidv4(),
-                name,
-                email
-            };
-            db.users.push(newUser);
-            return newUser;
-        },
-        deleteUser(_, { id }) {
-            const index = db.users.findIndex(user => user.id === id);
-            if (index === -1) {
-                throw new Error(`User with id ${id} not found`);
-            }
-            const deletedUser = db.users.splice(index, 1);
-            return deletedUser[0];
-        },
-    }
-}
+        ...addUserResolver,
+        ...deleteUserResolver,
+    },
+};
 
 const server = new ApolloServer({
     typeDefs,
